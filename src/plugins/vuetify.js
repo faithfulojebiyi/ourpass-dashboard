0973381c011b8bdc5178696713131f236948956a
Faithful Ojebiyi <faithfulojebiyi@gmail.com>
2021-08-15 20:48:32 +0100

    Feature: (Update vuetify themes)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,24 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#1BBC86',
+        secondary: '#26a69a',
+        third: '#FFFFFF',
+        accent: '#8FC461',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+        background: '#F7F7F7',
+        card: '#FF9803',
+        fab: '#1dc254',
+        loginbg: '#E4F7F2',
+        cardlight: '#FFF3E1',
+        redlight: '#FEE9E7'
+      }
+    }
+  }
 })
