42644f7062b41b5cbf1d0905f12aa0a9c5cd5a43
Faithful Ojebiyi <faithfulojebiyi@gmail.com>
2021-08-16 19:55:49 +0100

    Create GoBack.vue

---
+++ src/components/GoBack.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="flex d-inline-flex" @click="goBack">
+    <v-icon class="ml-n3 mr-2" size="40px" color="primary">mdi-chevron-left</v-icon>
+    <p class="d-sr-only">go back</p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'GoBack',
+  methods: {
+    goBack () {
+      return this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
