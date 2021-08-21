699ad8cb648dd87ac2f53d203b51fcf8e75c2588
Faithful Ojebiyi <faithfulojebiyi@gmail.com>
2021-08-21 12:17:28 +0100

    Feature: (support page)

---
+++ src/views/Support.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mx-2 px-sm-6 my-8">
+    <p class="text-h6 mt-1 mb-0">Support</p>
+    <p class="text-body-2 mt-1 p-family">You haven’t made any purchases with Pass. Checkout with Pass by exploring one of the stores below.</p>
+    <v-btn
+      class="no-text-transform px-8"
+      color="primary"
+      depressed
+    >
+      CHAT WITH US
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Support'
+}
+</script>
+
+<style scoped>
+.no-text-transform {
+  letter-spacing: 0.1px !important;
+}
+
+.card-class {
+  max-width: 529px;
+}
+
+</style>
